629d307b1c3e979e6d8f0f23cad48d965f12b37c
Mateusz Bogacz <mateusz.bogacz@cyfrowafoto.com>
2022-12-09 12:57:13 +0100

    Handling 2nd cart

--- js/counter.js
+++ js/counter.js
@@ -1,36 +1,35 @@
-const increasePrice = (element) => {
-  const currentValue = Number(element.value);
-  element.value = currentValue + 10;
-};
+// const increasePrice = (element) => {
+//   const currentValue = Number(element.value);
+//   element.value = currentValue + 10;
+// };
 
-const decreasePrice = (element) => {
-  const currentValue = Number(element.value);
-  element.value = currentValue - 10;
-};
+// const decreasePrice = (element) => {
+//   const currentValue = Number(element.value);
+//   element.value = currentValue - 10;
+// };
 
-const counterDisplayElem = document.querySelector(".counter-display");
-const counterMinusElem = document.querySelector(".counter-minus");
-const counterPlusElem = document.querySelector(".counter-plus");
+// const counterDisplayElem = document.querySelector(".counter-display");
+// const counterMinusElem = document.querySelector(".counter-minus");
+// const counterPlusElem = document.querySelector(".counter-plus");
 
-counterPlusElem.addEventListener("click", () => {
-  increasePrice(counterDisplayElem);
-});
+// counterPlusElem.addEventListener("click", () => {
+//   increasePrice(counterDisplayElem);
+// });
 
-counterMinusElem.addEventListener("click", () => {
-  if (counterDisplayElem.value <= 50) return;
-  decreasePrice(counterDisplayElem);
-});
+// counterMinusElem.addEventListener("click", () => {
+//   if (counterDisplayElem.value <= 50) return;
+//   decreasePrice(counterDisplayElem);
+// });
 
-const counterDisplayElem2 = document.querySelector(".counter-display2");
-const counterMinusElem2 = document.querySelector(".counter-minus2");
-const counterPlusElem2 = document.querySelector(".counter-plus2");
+// const counterDisplayElem2 = document.querySelector(".counter-display2");
+// const counterMinusElem2 = document.querySelector(".counter-minus2");
+// const counterPlusElem2 = document.querySelector(".counter-plus2");
 
-counterPlusElem2.addEventListener("click", () => {
-  increasePrice(counterDisplayElem2);
-});
+// counterPlusElem2.addEventListener("click", () => {
+//   increasePrice(counterDisplayElem2);
+// });
 
-counterMinusElem2.addEventListener("click", () => {
-  if (counterDisplayElem.value <= 250) return;
-  decreasePrice(counterDisplayElem2);
-});
-
+// counterMinusElem2.addEventListener("click", () => {
+//   if (counterDisplayElem.value <= 250) return;
+//   decreasePrice(counterDisplayElem2);
+// });
